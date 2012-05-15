a2ccf45492c04979937e5a67ff7cb59edcfbbc8c
Manoj Dayaram <i.am.noj@gmail.com>
2012-05-15 11:18:17 -0700

    Added brainstorming file for rwlock

---
+++ bstorm/rwlock.go
@@ -0,0 +1,64 @@
+// This go file was written as a test to see how much latency using read/write
+// locks adds to a simple variable read.  Running this program with and without
+// the use of read/write locks showed that the rw.RLock()/RUnlock() methods add
+// roughly 20ns of time to a read.
+//
+// This test was performed in order to judge the way we would implement the
+// ability to change priorities of processors on the fly.  We considered the
+// following implementations:
+//
+// 1)	Wrapping the priority field in the Processor with a RW lock and offering
+//		Setter/Getter methods for it.
+// 2)	Pushing the checking/filtering of the priority of a message to the 
+//		writer go routine which writes the message to the appropriate writer
+// 3)	Each go routine in the main application has its own logger and thus,
+//		will have to update the priority accordingly.  However, since the logger
+//		is only used in a single go routine, there's no race conditions in 
+//		simply updating the priority.
+//
+// We chose solution (1) after the test below since the penalty for adding
+// the RW locks is small and offers more flexibilty in terms of how a logger
+// can be used (aka, we can have one global logger for all go routines, etc...)
+//
+// Solution (2) is elegant in the sense that it would require no extra locking,
+// however, it would mean that the application as a whole would have to do more
+// work as messages that would be filtered out before hand will now have to 
+// go through the log channel (then to get filtered out).
+//
+package main
+
+import (
+	"time"
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	teapot := 8675309
+//	rw := &sync.RWMutex{}
+	starter, finisher := &sync.WaitGroup{}, &sync.WaitGroup{}
+	starter.Add(1)
+	for i := 0; i < 20000; i++ {
+		finisher.Add(1)
+		go func () {
+			var sum int64 = 0
+			j := 0
+			starter.Wait()
+			for ; j < 10000000; j++ {
+				now := time.Now()
+//				rw.RLock()
+				_ = teapot
+//				rw.RUnlock()
+				duration := time.Now().Sub(now)
+				sum += duration.Nanoseconds()
+			}
+			avg := float64(sum)/float64(j)
+			fmt.Printf("Avg: %fns\n", avg)
+			finisher.Done()
+		}()
+	}
+	starter.Done()
+	finisher.Wait()
+}
+
